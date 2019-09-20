35be4819591e8aed68c2b87a3007f6ade0bc5c8f
Harish Ananthakrishnan <hanantha@qti.qualcomm.com>
2019-09-20 15:32:43 -0700

    Upgraded to latest Fabric plugin

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,27 @@
 #include <jni.h>
 #include <string>
 
+void third(){
+    assert(1==0);
+}
+
+int second(){
+    third();
+    return 2;
+}
+
+int first(){
+    second();
+    return 1;
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_harndkcrash_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    first();
     return env->NewStringUTF(hello.c_str());
 }
+
+
--- app/src/main/java/com/example/harndkcrash/MainActivity.java
+++ app/src/main/java/com/example/harndkcrash/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.harndkcrash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,8 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        final TextView tv = findViewById(R.id.sample_text);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText(stringFromJNI());
+            }
+        });
     }
 
     /**
